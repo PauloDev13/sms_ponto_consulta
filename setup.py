f714ea3ecc6e1806283cac848475a8391bfc3438
prmorais <prmorais1302@gmail.com>
2024-07-30 22:27:58 -0300

    Refactoring setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ build_exe_options = {
     "include_files": [
         ('utils', 'utils'),
         ('services', 'services'),
+        ('PLANILHAS_SMS', 'PLANILHAS_SMS'),
         ('.env', '.env'),
         ('main.py', 'main.py'),
         ('start_streamlit.py', 'start_streamlit.py')
@@ -30,7 +31,7 @@ executables = [
     Executable(
         script="start_streamlit.py",
         base="Win32GUI" if sys.platform == "win32" else None,
-        target_name="ponto_sms_app.exe"
+        target_name="app_sms.exe"
     )
 ]
 
