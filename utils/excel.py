91aacd195fee68a7d1ce72cb0319de7b0868d930
prmorais <prmorais1302@gmail.com>
2024-08-02 12:48:03 -0300

    Completing the refactoring of the format_excel module

--- utils/excel.py
+++ utils/excel.py
@@ -15,15 +15,17 @@ if not file_path:
 
 # Cria o arquivo Excel
 def generate_excel_file(data_dic: Dict[int, pd.DataFrame], employee_name: str, cpf: str):
+    # Monta uma string concatenando o caminho e nome do arquivo excel
     file_name = os.path.join(file_path, f'{employee_name} - CPF_{cpf}.xlsx')
+
     # Usa o metódo ExcelWriter do Pandas para criar o arquivo
     # Excel usando a biblioteca xlsxwriter
     with pd.ExcelWriter(file_name, engine='xlsxwriter') as writer:
         # Itera sobre o dicionário (data_dic) extraindo o ano (year)
-        # e o Dataframe com os dados dos meses do respectivo ano
+        # e o Dataframe com os dados dos meses de cada ano
         for year, df_year in data_dic.items():
             # Cria a planilha com abas para cada ano, começando
-            # na linha índice 0do Dataframe, sem trazer os índices
+            # na linha índice 0 do Dataframe, sem trazer os índices
             # das linha e o cabeçalho do Dataframe
             df_year.to_excel(writer, sheet_name=str(year), index=False, startrow=0, header=False)
             workbook = writer.book
@@ -35,7 +37,7 @@ def generate_excel_file(data_dic: Dict[int, pd.DataFrame], employee_name: str, c
             formats = format_excel.define_formats(workbook)
 
             # Chama a função (apply_formatting) do módulo (format_excel),
-            # passando as planilhas (worksheet, o Dataframe (df_year) e a
-            # variável (formats) definida anteriorment. Essa função aplica
+            # passando a planilha (worksheet), o Dataframe (df_year) e a
+            # variável (formats) definida anteriormente. Essa função aplica
             # as formatações nas planilhas que serão salvas no arquivo Excel.
             format_excel.apply_formatting(worksheet, df_year, formats)
--- utils/format_excel.py
+++ utils/format_excel.py
@@ -93,37 +93,42 @@ def define_formats(workbook):
 
 
 # Função que implementa condicionais e formatação
-# (usando a função define_formats) em células individuais do arquivo Excel
+# (usando a função define_formats), em células individuais do arquivo Excel
 def apply_formatting(worksheet, df_year, formats):
+    # Formata as larguras das colunas e aplica em algumas
+    # delas a formatação que centraliza o conteúdo
     worksheet.set_column(0, 0, 25)
     worksheet.set_column(1, 1, 20, formats['col_center'])
     worksheet.set_column(2, 2, 25)
     worksheet.set_column(3, 6, 20, formats['col_center'])
     worksheet.set_column(7, 10, 5, formats['col_center'])
 
+    # Define a variável (last_header_index) com valor -1.
+    # Essa variável vai pegar o número da linha imediatamente
+    # após a linha do cabeçalho
     last_header_index = -1
 
     # Itera sobre as linhas do Dataframe (df_year) criando
     #  uma (Series) com os índices e o conteúdo de cada linha
     for row_index, row in df_year.iterrows():
-
-
+        # Se o conteúdo da coluna índice 0 for igual a string 'DATA ENTRADA',
+        # atribui a variável (last_header_index) o valor do índice da
+        # última linha de cada mês
         if row.iloc[0] == 'DATA ENTRADA':
-            # Se o conteúdo da coluna índice 0 for igual a string 'DATA ENTRADA',
-            # atribui a variável (last_header_index) o valor do índice da
-            # última linha de cada mês
             last_header_index = row_index
 
-
+        # Se o tamanho do conteúdo das células da coluna 1 for maior que zero
+        # e menor ou igual a 8, aplica bordas em todas as células da coluna
         elif row.iloc[1] and 0 < len(row.iloc[1]) <= 8:
             worksheet.write(row_index, 1, row.iloc[1], formats['single_border'])
 
-
+        # Se na coluna índice zero os valores das células forem diferentes de
+        #  'JUSTIFICATIVA' ou 'AVISO', aplica bordas em todas as células da coluna
         elif row.iloc[0] not in ['JUSTIFICATIVA', 'AVISO']:
             worksheet.write(row_index, 0, row.iloc[0], formats['single_border'])
 
-        # Se na coluna índice 0 o conteúdo da célula form igual a
-        # 'JUSTIFICATIVA' ou 'AVISO', aplica formatação
+        # Se na coluna índice 0 o conteúdo da célula for igual a 'JUSTIFICATIVA'
+        # ou 'AVISO', aplica bordas customizadas em todas as células da coluna
         elif row.iloc[0] in ['JUSTIFICATIVA', 'AVISO']:
             worksheet.write(row_index, 0, row.iloc[0], formats['custom_1'])
             # Se na coluna índice 1 o conteúdo da célula for >= a 145
@@ -134,32 +139,34 @@ def apply_formatting(worksheet, df_year, formats):
             #  >= a 381, aumenta a altura da linha para 50
             elif len(row.iloc[1]) >= 381:
                 worksheet.set_row(row_index, 50)
-            # Mescla as células da linha entre as colunas com
-            # índices 1 a 6 e aplica formatação
+            # Mescla as células da linha entre as colunas com índices de 1 a 6 e
+            # aplica bordas customizadas em todas as células da coluna
             worksheet.merge_range(
                 row_index, 1, row_index, 6, row.iloc[1], formats['custom_2'])
 
         # Itera sobre as linhas criando uma (Series) que
         # contém o índice e os valores em cada coluna por linha
         for col_index, value in enumerate(row):
-            # Se o índice da coluna for == a 0 e o valor da célula começar
-            #  com a string 'PONTO DIGITAL', mescla as células de toda a linha
+            # Se o índice da coluna for == a 0 e o valor da célula começar com a
+            #  string 'PONTO DIGITAL', mescla as células da coluna em toda a linha
             if col_index == 0 and value.startswith('PONTO DIGITAL'):
                 worksheet.merge_range(
                     row_index, 0, row_index, 10, value, formats['header'])
 
             # Se a string 'DATA ENTRADA' estiver em alguma
-            #  célula da coluna, aplica formatação
+            #  célula da coluna, aplica a formatação 'header'
             elif 'DATA ENTRADA' in row.values:
                 worksheet.write(row_index, col_index, row.iloc[col_index], formats['header'])
 
-            # Se o índice da coluna for == a 0 e o valor da célula começar
-            # com a string 'NÃO HÁ REGISTRO', mescla as células de toda a linha
+            # Se o índice da coluna for == a 0 e o valor da célula começar com a
+            # string 'NÃO HÁ REGISTRO', mescla as células da coluna em toda a linha
             elif col_index == 0 and value.startswith('NÃO HÁ REGISTRO'):
                 worksheet.set_row(row_index, 30)
                 worksheet.merge_range(
                     row_index, 0, row_index, 10, value, formats['warning'])
 
+            # Insere bordas nas células das colunas com índices
+            # maior que 1 e menor que 4 (colunas 2 e 3)
             elif 1 < col_index < 4:
                 worksheet.write(row_index, col_index, value, formats['single_border'])
 
@@ -175,28 +182,28 @@ def apply_formatting(worksheet, df_year, formats):
                   and value < '12:00:00' and value not in ['---', '']):
                 worksheet.write(row_index, col_index, value, formats['blue_bold'])
 
-            # Se a coluna é índice 6, aplica formatação
-            # insere bordas em todas as células da coluna
+            # Se o índice da coluna é igual 6, aplica formatação
+            # que insere bordas em todas as células da coluna
             elif col_index == 5:
                 worksheet.write(row_index, col_index, value, formats['single_border'])
 
-            # Se na coluna índice 6 o valor da célula for == a
+            # Se o índice da coluna é igual 6 e o valor da célula for == a
             # 'APROVADO', aplica formatação background verde e fonte branca.
             elif col_index == 6 and value == 'APROVADO':
                 worksheet.write(row_index, col_index, value, formats['green_bold'])
 
-            # Se na coluna índice 6 o valor da célula for == 'REPROVADO' ou
-            # 'ESPERA', aplica formatação background vermelho e fonte branca.
+            # Se o índice da coluna é igual 6 e o valor da célula for == 'REPROVADO'
+            #  ou 'ESPERA', aplica formatação background vermelho e fonte branca.
             elif col_index == 6 and value == 'REPROVADO' or value == 'ESPERA':
                 worksheet.write(row_index, col_index, value, formats['red_bold'])
 
-            # Se a coluna é índice 6, aplica formatação
-            # insere bordas em todas as células da coluna
+            # Se o índice da coluna é igual 6, aplica formatação
+            # que insere bordas em todas as células da coluna
             elif col_index == 6:
                 worksheet.write(row_index, col_index, value, formats['single_border'])
 
             # Se o índice da coluna for > 6 e < 11, aplica a formação que
-            # insere bordas em todas as células do intervalo de colunas
+            # insere bordas em todas as células do intervalo em cada coluna
             elif col_index > 6:
                 worksheet.conditional_format(f'H{row_index}:K{row_index}', {
                     'type': 'cell',
@@ -205,15 +212,19 @@ def apply_formatting(worksheet, df_year, formats):
                     'format': formats['all_borders']
                 })
 
-
+            # Se nas colunas existir uma célula com valor igual a 'TOTAIS'...
             elif value == 'TOTAIS':
-                # Se nas colunas existir uma célula com valor igual a 'TOTAIS',
-                # mescla todas as células da linha e aplica formatação
+                # mescla as células das colunas de 0 a 6 e aplica formatação
                 worksheet.merge_range(
                     row_index, 0, row_index, 6, row.iloc[0], formats['col_total'])
 
+                # Pega o número de colunas mescladas e atribui a variável (formula_col)
                 merged_cells = 7
                 formula_col = merged_cells
+
+                # Se a variável (last_header_index) definita anteriormente no início da função
+                # for >= 0, seta a variável (start_row) com o valor de (last_header_index) + 1,
+                # se não, seta (start_row) = 1
 
                 # start_row = last_header_index + 1 if last_header_index >= 0 else 1
                 if last_header_index >= 0:
@@ -221,18 +232,24 @@ def apply_formatting(worksheet, df_year, formats):
                 else:
                     start_row = 1
 
+                # Define que a variável (end_row) é igual ao índice da linha atual menos 1
                 end_row = row_index - 1
 
+                # Se a (start_row) for <= (end_row)
                 if start_row <= end_row:
-                    start_cell = xlsxwriter.utility.xl_rowcol_to_cell(start_row, formula_col)
-                    end_cell = xlsxwriter.utility.xl_rowcol_to_cell(end_row, formula_col)
-
+                    # Itera sobre o número de colunas que contém as células a serem somadas
                     for index in range(0, 4):
+                        # Cria os intervalos inicial e final com as células que
+                        # terão seus conteúdos somados
                         start_cell = xlsxwriter.utility.xl_rowcol_to_cell(start_row, formula_col + index)
                         end_cell = xlsxwriter.utility.xl_rowcol_to_cell(end_row, formula_col + index)
 
+                        # Monta a fórmula que será usada para a soma com os intervalos de células
+                        # definidos nas linhas de código anteriores
                         formula = f'=SUM({start_cell}:{end_cell})'
 
+                        # Aplica a fórmula na célula de cada coluna onde ficará o
+                        # resultado com o total da soma dos intervalos de células
                         worksheet.write_array_formula(
                             end_row + 1,
                             formula_col + index,
@@ -241,6 +258,7 @@ def apply_formatting(worksheet, df_year, formats):
                             formula,
                             formats['results']
                         )
+
 
                 else:
                     worksheet.write(row_index, formula_col, 0)
